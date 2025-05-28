6c84b664b1523c5c318107c5a8146cf50b458ddb
Nicolas Cordoba <nico343@outlook.es>
2025-05-28 20:46:24 -0300

    optimized a bit of the scrap

--- plugins/scrap.py
+++ plugins/scrap.py
@@ -1,53 +1,63 @@
-from playwright.sync_api import sync_playwright, Playwright, Page
+from playwright.sync_api import sync_playwright, Page
+from bs4 import BeautifulSoup
 
-def pagination(url, page: Page, browser ,logger,):
+def pagination(url, page: Page, logger):
     try:
         page.goto(url)
+        page.route("**/*", lambda route, request: route.abort() if request.resource_type in ["image", "font", "media"] else route.continue_())
+        
         page.wait_for_selector("ul.pagination.pagination--links")
         number_of_pages = page.locator("li.pagination__page").nth(-2).inner_text()
-        logger.info(f"Number of pages: {number_of_pages}")
-    except:
+    except Exception as e:
         logger.error("Error: No se pudo cargar la pagina")
-        browser.close()
-        raise Exception("Error: No se pudo cargar la pagina")
+        raise
+    
     properties = []
     logger.info(f"Starting to scrape {number_of_pages} pages")
-    for i in range(1, int(number_of_pages)):
-        logger.info(f"Scraping page {i}")
-        # scrap_rsarg scraps the data of each property
-        data = scrap_rsarg(url + f"?pagina-{i}", page, browser, logger)
+    for i in range(1, int(number_of_pages) + 1):
+        data = scrap_propertie_details(url + f"?pagina-{i}", page, logger)
         properties.extend(data)
+        
+        # Scraping 40 pages as limit to avoid overloading the server
         if i >= 40:
             logger.info("40 paginaciónes alcanzadas, deteniendo la paginación")
             break
     return properties
 
 
-def scrap_rsarg(url:str,page: Page,browser, logger):
-    try:    
-        if url[-1] != '1':
-            page.goto(url)
-            # logger.info(f"Scraping page {url[-1]}")
+def scrap_propertie_details(url:str,page: Page, logger):
+    try:
+        if page.is_closed():
+            logger.warning("Página cerrada, reabriendo nueva página")
+            page = page.context.new_page()
 
-        page.wait_for_selector(".listing__items")   
+        page.goto(url, timeout=15000)
+        page.wait_for_selector(".listing__items", timeout=15000)
         items = page.locator(".listing__item")
-    except:
-        logger.error("Error: No se pudo cargar la pagina")
-        browser.close()
-        raise Exception("Error: No se pudo cargar la pagina")
+    except Exception as e:
+        logger.error(f"Error: No se pudo cargar la página {url} (linea 36): {e}")
+        raise
+    
     items_count = items.count()
     properties = []
-    # logger.info("Starting to add items to list")
+
     for i in range(items_count):
         item = items.nth(i)
-        adress = item.locator("p.card__address").inner_text()
-        city = item.locator("p.card__title\\--primary").inner_text()
-        price_currency = item.locator("p.card__price").inner_text()
-        features = item.locator("ul.card__main-features").inner_text()
-        title = item.locator("h2.card__title").inner_text()
-        info = item.locator("p.card__info").inner_text()
-        # text = item.inner_text()
-        id = item.get_attribute("id")
+        
+        
+        item_html = item.inner_html()
+        soup = BeautifulSoup(item_html, "html.parser")
+        
+        def get_text(sel):
+            tag = soup.select_one(sel)
+            return tag.get_text(strip=True) if tag else None
+        id = soup.select_one("[data-item-card]")["data-item-card"] if soup.select_one("[id]") else None
+        adress= get_text("p.card__address") 
+        city = get_text("p.card__title--primary")
+        price_currency = get_text("p.card__price")
+        features = get_text("ul.card__main-features")
+        title = get_text("h2.card__title")
+        info = get_text("p.card__info") 
         properties.append({
             "adress": adress,
             "city": city,
@@ -57,8 +67,8 @@ def scrap_rsarg(url:str,page: Page,browser, logger):
             "info": info,
             "id": id
         })
-        # logger.info(f"Item {id} added")
     return properties
+
 
 def run(url: str, logger = None, province: str = "tucuman") -> list[dict]:
     if logger is None:
@@ -75,23 +85,29 @@ def run(url: str, logger = None, province: str = "tucuman") -> list[dict]:
             "--disable-background-networking",
             "--disable-sync",
             "--disable-default-apps",
-            "--mute-audio"
+            "--mute-audio",
+            # "--single-process" # Muy importante en instancias con poca RAM # DISCLAIMER: Parece cerrar el navegador
+            "--no-zygote"
+            
         ])
         context = browser.new_context(
                 user_agent="Mozilla/5.0 (Windows NT 11.0; Win64; x64)...",
                 viewport={"width": 1280, "height": 800},
                 locale="en-US"
             )
+        # context.set_default_timeout(10000)
+        # context.set_default_navigation_timeout(10000)
         logger.info("Browser launched")
         page = context.new_page()
-        # page.goto("http://example.com")
+
         url = url + province + "-arg"
         # Pagination gets the number of pages and scrapes each page
-        data = pagination(url,page, browser, logger)
+        data = pagination(url, page, logger)
         browser.close()
-    logger.info(f"Scraping completed, found {len(data)} of properties in {province}") 
     if len(data) < 1:
         logger.info(f"No data found")
         raise Exception("No data found")
+    
+    logger.info(f"Scraping completed, found {len(data)} of properties in {province}") 
     return data
 
--- tasks/extract.py
+++ tasks/extract.py
@@ -1,8 +1,23 @@
 from prefect import task, get_run_logger
 from plugins.scrap import run as scrap_run
+from datetime import datetime
+
+PROVINCE_BY_DAY = {
+    1: "tucuman",
+    2: "cordoba",
+    3: "mendoza",
+    4: "santa-fe",
+}
+
+def get_province_by_day() -> str:
+    """
+    Returns the province based on the day of the month.
+    """
+    today = datetime.today().day
+    return PROVINCE_BY_DAY.get(today, "tucuman")
 
 @task
-def scrap_data(url: str, province: str = "tucuman"):
+def scrap_data(url: str, province: str = get_province_by_day()):
     try:
         logger = get_run_logger()
         data = scrap_run(url, logger, province)
