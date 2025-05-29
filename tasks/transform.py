9bb105fb66ba3bd71d197b133c18c347ad5fd252
Nicolas Cordoba <nico343@outlook.es>
2025-05-28 23:04:13 -0300

    scrap was completed, but now an error during transformation

--- tasks/transform.py
+++ tasks/transform.py
@@ -10,15 +10,13 @@ def transform_data(data: list[dict]):
             transformed_item = {
                 "address": item.get("adress"),
                 "city": item.get("city"),
-                "currency": item.get("price_currency").split(" ")[0],
-                "price": item.get("price_currency").split(" ")[1],
+                "price_currency": item.get("price_currency"),
                 "features": item.get("features"),
                 "title": item.get("title"),
                 "info": item.get("info"),
                 "id": item.get("id")
             }
             transformed_data.append(transformed_item)
-            logger.info(f"Transformed item with ID: {item.get('id')}")
         except Exception as e:
             logger.error(f"Error transforming item with ID: {item.get('id')}")
             raise e
