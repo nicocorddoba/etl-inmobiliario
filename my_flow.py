279d2a4b08df0e9bb80a8c9c7a7a4410aec33189
Nicolas Cordoba <nico343@outlook.es>
2025-06-18 18:45:56 -0300

    sm-api obtains fb creds with os.getenv

--- my_flow.py
+++ my_flow.py
@@ -37,8 +37,6 @@ if __name__ == "__main__":
     # Deploying the load flow for the API
     FB_URL = os.getenv("FB_URL")
     API_URL = os.getenv("API_URL")
-    FB_EMAIL = os.getenv("FB_EMAIL")
-    FB_PASSWORD = os.getenv("FB_PASSWORD")
     flow.from_source(
         source=repo_api,
         entrypoint="main_etl.py:flujo_carga_api"
@@ -46,9 +44,7 @@ if __name__ == "__main__":
         name="sm-api",
         parameters={
             "api_url": API_URL,
-            "fb_url": FB_URL,
-            "fb_email": FB_EMAIL,
-            "fb_password": FB_PASSWORD
+            "fb_url": FB_URL
         },
         work_pool_name="ec2-work-pool",
         cron = "0 12 * * 2"
